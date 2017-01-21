ad6275fde0e1510a803d841e99f6f02f37646c10
Arthelon <hsing.daniel@gmail.com>
2017-01-22 03:05:23 +0800

    Updated ts endpoint
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- routes/ts.js
+++ routes/ts.js
@@ -4,9 +4,14 @@ const Errors = require('../db').Errors;
 const jsonResponse = require('../utils/api').jsonResponse;
 
 Router.get('/:id', (req, res, next) => {
+    let offset = req.query.offset || 0;
     Feed.get(req.params.id).getJoin({ ts: true }).run()
         .then((feed) => {
-            jsonResponse(res, true, feed.ts || [], 'Successfully retrieved ts');
+            const length = feed.ts ? feed.ts.length : 0;
+            if (offset >= length) {
+                offset = 0
+            }
+            jsonResponse(res, true, feed.ts[offset] || [], 'Successfully retrieved ts');
         })
         .catch(Errors.DocumentNotFound, () => {
            jsonResponse(res, false, [], 'TS not found', 404);
