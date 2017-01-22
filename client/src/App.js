466190a6cdcf6eebfc0996a0b70f555d0987dc04
Arthelon <hsing.daniel@gmail.com>
2017-01-22 10:02:31 +0800

    Throttled toast
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,27 @@ import { FEED_EVENTS } from '../../constants';
 import { RISK_THRESHOLD } from './constants';
 import { Toaster, Position, Intent } from '@blueprintjs/core'
 import io from './io';
+import * as _ from 'lodash';
 import './styles.css'
 
 
 class App extends Component {
 
     componentDidMount() {
-        io.on(FEED_EVENTS.update, this.handleUpdate);
+        io.on(FEED_EVENTS.update, _.throttle(this.handleUpdate.bind(this), 5000));
     }
 
     handleUpdate = (feed) => {
         feed = JSON.parse(feed);
-        // if (feed.risk >= RISK_THRESHOLD) {
-        //     const toast = Toaster.create({
-        //         position: Position.TOP,
-        //     });
-        //     toast.show({
-        //         message: `Warning, high probability of violence in ${feed.name}`,
-        //         intent: Intent.DANGER
-        //     })
-        // }
+        const toast = Toaster.create({
+            inline: true,
+            position: Position.TOP,
+        });
+        toast.show({
+            message: `Warning, high probability of violence in ${feed.name}`,
+            intent: Intent.DANGER,
+            timeout: 4000
+        });
     };
 
     render() {
